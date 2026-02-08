b1c7d3a7afff4fadccd8da07a2d385ee48ea95b1
Daniel <danieljproctor94@gmail.com>
2026-02-08 12:15:42 +0700

    feat: show date on mobile dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -228,6 +228,7 @@ export default function Dashboard() {
                     ) : (
                         <>
                             <h1 className="text-3xl font-bold tracking-tight">Hi {userProfile.displayName?.split(' ')[0] || "User"}, welcome back!</h1>
+                            <p className="text-muted-foreground block md:hidden">{format(currentDate, "EEEE, MMMM do, yyyy")}</p>
                         </>
                     )}
                 </div>
