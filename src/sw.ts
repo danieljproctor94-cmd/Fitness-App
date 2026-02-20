192ecdd124cb691e429ca3b8fc815dc5ecdde239
Daniel <danieljproctor94@gmail.com>
2026-02-20 13:45:41 +0100

    Restore SKIP_WAITING event listener to allow old PWA clients to manually trigger an update

--- src/sw.ts
+++ src/sw.ts
@@ -22,7 +22,11 @@ try {
   console.warn('Error setting up nav route', error)
 }
 
-self.skipWaiting();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 clientsClaim();
 
